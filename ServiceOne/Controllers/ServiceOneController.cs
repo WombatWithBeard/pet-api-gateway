8f46bda181e46fdafaad522befda37793b12dc74
Anton borodin <boroda132@hotmail.com>
2019-12-08 15:26:17 +0300

    Init

---
+++ ServiceOne/Controllers/ServiceOneController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ServiceOne.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ServiceOneController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<ServiceOneController> _logger;
+
+        public ServiceOneController(ILogger<ServiceOneController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "ServiceOne";
+        }
+    }
+}
---
+++ ServiceTwo/Controllers/ServiceTwoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ServiceTwo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ServiceTwoController : ControllerBase
+    {
+        private readonly ILogger<ServiceTwoController> _logger;
+
+        public ServiceTwoController(ILogger<ServiceTwoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "ServiceTwo";
+        }
+    }
+}
